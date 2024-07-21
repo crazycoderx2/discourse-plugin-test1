3e3ca70c40141ad7a256d2ee116e722c36d38bb8
crazycoderx2 <crazycoderx@gmail.com>
2024-07-21 17:42:43 +0200

    Update test1.js

--- assets/javascripts/discourse/initializers/test1.js
+++ assets/javascripts/discourse/initializers/test1.js
@@ -2,9 +2,9 @@ export default {
   name: 'alert',
   initialize() {
     window.onmessage = function(e) {
-      if (e.data == 'IFramePopup') {
+      if (e.data === 'IFramePopup') {
         alert('It works IFramePopup!');
       }
-    }
+    };
   }
 };
